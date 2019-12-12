f1913b7fb203ffaa6673e60cbaa45f9b18d55f34
iamcristos <icpengineer@intcrzyppl.com>
2019-12-12 19:10:44 +0100

    added watch to false

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import './App.css'
 
 function App() {
 	return (
-  <div className='App'>
-  <header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-      <a
-  className='App-link'
-  href='https://reactjs.org'
-  target='_blank'
-  rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+			<div className='App'>
+				<header className='App-header'>
+					<img src={logo} className='App-logo' alt='logo' />
+					<a
+						className='App-link'
+						href='https://reactjs.org'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+								Learn React
+					</a>
+				</header>
+			</div>
 	)
 }
 
