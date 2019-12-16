cc4fdb5cc49f3118ac64488a8037781848b9c73b
Rory Flint <rory@roryflint.co.uk>
2019-12-16 17:48:43 +0000

    Add tests for landing page

---
+++ src/Views/LandingPage.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { render } from '@testing-library/react'
+import LandingPage from './LandingPage'
+
+test('renders all 6 sub components', () => {
+    const { container, getByText } = render(<Router><LandingPage /></Router>)
+    const heroText = getByText(/BUY ART/i)
+    
+    expect(container.children.length).toBe(6);
+    expect(heroText).toBeInTheDocument();
+})
+
