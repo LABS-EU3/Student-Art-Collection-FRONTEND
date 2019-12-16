e786720e474f0ce94bef2404b4773c86c3ff11a1
Pere <pere.solaclaver@gmail.com>
2019-12-16 22:47:08 +0000

    Create theme with Theme provider of styled components. This will allow us to set theme styles that can be later reused.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react'
-import './App.css'
-import LandingPage from './Views/LandingPage'
+import React from 'react';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import './App.css';
+import LandingPage from './Views/LandingPage';
+
+const theme = {
+  grayDark: '#7B7A7A',
+  grayDark: '#A5A5A5',
+  black: '#000000',
+  backgroundGreen: '#0F9D58',
+  backgroundGreyForm: 'rgba(196, 196, 196, 0.2)',
+  backgroundGrey: '#C4C4C4',
+  orangeCancelButton: '#FFA439',
+  greenSaveButton: 'rgba(49, 215, 21, 0.68)',
+  orangeChangePic: '#FD7135',
+  red: 'red'
+};
 
 function App() {
-	return (
-		<LandingPage />
-	)
+  return (
+    <ThemeProvider theme={theme}>
+      <LandingPage />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
