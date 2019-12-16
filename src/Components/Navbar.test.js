b8b13783c055964d2395a2a2d1e50b1bc57f0681
Rory Flint <rory@roryflint.co.uk>
2019-12-16 16:11:31 +0000

    Fix conflict

--- src/Components/Navbar.test.js
+++ src/Components/Navbar.test.js
@@ -4,7 +4,6 @@ import { render } from '@testing-library/react'
 import NavBar from './NavBar'
 
 test('renders correct title in NavBar', () => {
-	const { getByText } = render(<Router><NavBar /></Router>)
     const { getByText } = render(<Router><NavBar /></Router>)
     const mainTitle = getByText(/artFunder/i)
     expect(mainTitle).toBeInTheDocument()
