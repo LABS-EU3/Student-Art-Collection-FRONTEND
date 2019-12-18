cdd243e6c25da0e796c1f10497c68dc8c8a08ce9
James Grantham <james.r.grantham@gmail.com>
2019-12-18 09:02:36 +0000

    Set up Register form testing

--- src/Views/Register.test.js
+++ src/Views/Register.test.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { render } from '@testing-library/react'
-import NavBar from './NavBar'
+import Register from './Register'
 
-test('renders correct title in NavBar', () => {
-    const { getByText } = render(<Router><NavBar /></Router>)
-    const mainTitle = getByText(/artFunder/i)
+test('renders correct title', () => {
+    const { getByText } = render(<Router><Register /></Router>)
+    const mainTitle = getByText(/register/i)
     expect(mainTitle).toBeInTheDocument()
 })
 
-test('navigation class exists', () => {
-    const { container } = render(<Router><NavBar /></Router>)
-    const navigation = container.querySelector('.navigation')
+test('button with toggleButton class exists', () => {
+    const { container } = render(<Router><Register /></Router>)
+    const navigation = container.querySelector('.toggleButton')
     expect(navigation).not.toBe(null)
 })
 
-test('nav element has five children', () => {
-    const { container } = render(<Router><NavBar /></Router>)
-    const navigation = container.querySelector('nav')
-    expect(navigation.children.length).toBe(5)
+test('renders the form', () => {
+  const { container, getByText } = render(<Router><Register /></Router>)
+  const passwordField = getByText(/password/i)
+  const emailField = getByText(/e-mail/i)
+  
+  expect(container.children.length).toBe(1);
+  expect(passwordField).toBeInTheDocument();
+  expect(emailField).toBeInTheDocument();
 })
+
+
