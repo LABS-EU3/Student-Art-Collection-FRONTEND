9263bcc218bda06c8fc3defef5783824e7ea3057
James Grantham <james.r.grantham@gmail.com>
2019-12-19 21:21:25 +0000

    Add responsive border to form for larger screens

--- src/Views/Register.js
+++ src/Views/Register.js
@@ -38,18 +38,32 @@ export default connect(state => state, actionCreators)(Register)
 
 const Container = styled.div`
   max-width: 800px;
-  margin: 50px Auto;
+  margin: 30px Auto;
   display: flex;
   flex-direction: column;
   align-items: center;
   // justify-content: center;
-  font-family: ‘Roboto’, sans-serif
-  // border: solid 1px red
+  font-family: ‘Roboto’, sans-serif;
+
+  @media (min-width: 500px) {
+    margin-top: 100px;
+  }
+
+  @media (min-width: 1000px) {
+    padding: 80px 20px;
+    border-radius: 10px;
+    border: solid 0.5px lightgrey;
+  }
+
+  @media (min-width: 1500px) {
+    max-width: 1200px;
+    padding: 80px 50px;
+  }
 
   .title {
     display flex;
     flex-direction row;
-    justify-content: space-between
+    justify-content: space-between;
     width: 80%;
 
     @media (max-width: 500px) {
