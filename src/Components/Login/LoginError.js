450eb2e9c6d4fee0f355b9497036b569237c802a
Pere <pere.solaclaver@gmail.com>
2019-12-20 09:37:14 +0100

    Add minor styling to error message

--- src/Components/Login/LoginError.js
+++ src/Components/Login/LoginError.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 
 import * as actionCreators from '../../store/Actions/actionCreators';
 
-const LoginError = ({logInError}) => {
-    if (!logInError) {
-        return null
-    }
-return <div>{logInError.errorMessage}</div>;
+const StyledDiv = styled.div`
+  color: red;
+  font-size: 2rem;
+`;
+
+const LoginError = ({ logInError }) => {
+  if (!logInError) {
+    return null;
+  }
+  return <StyledDiv>{logInError.errorMessage}</StyledDiv>;
 };
 
 export default connect(state => state, actionCreators)(LoginError);
