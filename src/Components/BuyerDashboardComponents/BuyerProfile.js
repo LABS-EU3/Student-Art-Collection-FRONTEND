88d178a0e0f78c21879bb13fec221217bfcc5ab8
Rory Flint <rory@roryflint.co.uk>
2019-12-20 11:10:50 +0000

    update

--- src/Components/BuyerDashboardComponents/BuyerProfile.js
+++ src/Components/BuyerDashboardComponents/BuyerProfile.js
@@ -1,14 +1,34 @@
 import React from 'react'
 import styled from 'styled-components'
+import ProfilePhoto from '../../Assets/profile.jpg'
 
 const ProfileContainer = styled.div`
 width: 90%;
 height: 90%;
 border: 1px solid red;
+display: flex;
+flex-direction: column;
+align-items: center;
+
+    .photo-container {
+        width: 110px;
+        height: 110px;
+        border-radius: 50%;
+
+         img {
+             max-width: 100%;
+             height: auto;
+             border-radius: 50%;
+         }
+    }
 `
 function BuyerProfile() {
     return (
-        <ProfileContainer/>
+        <ProfileContainer>
+            <div className="photo-container">
+                <img src={ProfilePhoto} alt='Users profile' />
+            </div>
+        </ProfileContainer>
     )
 }
 
