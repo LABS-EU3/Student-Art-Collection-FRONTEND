4c8c506ddb431e22076149257c68521a4e3ebacb
Rory Flint <rory@roryflint.co.uk>
2020-01-10 17:32:58 +0000

    Add Spinner and working endpoint

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -4,8 +4,11 @@ import * as yup from 'yup';
 import HeroImage from '../Assets/contactus.svg'
 import SuccessImage from '../Assets/success1.svg'
 import ContactForm from '../Components/ContactForm/ContactForm';
+import Spinner from '../Components/Spinner';
+import { axiosWithBase } from '../AxiosCustom';
+import { ToastContainer, toast } from 'react-toastify';
 
-import { StyledContact, StyledForm, StyledSuccess } from './ContactStyle';
+import { StyledContact, StyledForm, StyledSuccess, StyledSpinner } from './ContactStyle';
 
 const validationSchema = yup.object().shape({
     email: yup
@@ -29,34 +32,54 @@ const initialValues = {
 
 function ContactPage() {
     const [submmited, setSubmitted] = useState(false);
+    const [spinning, setSpinning] = useState(false)
 
     const submitMessage = (values, actions) => {
-        //Console log temporary until endpoit is working
-        console.log(values);
-        actions.resetForm();
-        setSubmitted(true);
+        setSpinning(true);
+        axiosWithBase()
+            .post('/contactus', values)
+            .then(() => {
+                actions.resetForm();
+                setSpinning(false);
+                setSubmitted(true);
+            })
+            .catch(() => {
+                setSpinning(false);
+                toast.error("Error submitting message.")
+            })
     };
     return (
         <StyledContact>
             <div className="contactContainer">
                 <div className="contactLeft">
-                    <h1>Contact Us</h1>
-                    <p>Want to get in touch? Use the form below and we will get back to you
-                        as soon as possible.
+                    {spinning ? <StyledSpinner><Spinner /></StyledSpinner>
+                        :
+                        <>
+                            <h1>Contact Us</h1>
+                            <p>Want to get in touch? Use the form below and we will get back to you
+                                as soon as possible.
                     </p>
-                    {!submmited ?
-                        <StyledForm>
-                            <Formik
-                                initialValues={initialValues}
-                                validationSchema={validationSchema}
-                                onSubmit={submitMessage}
-                                component={ContactForm}
+                            {!submmited ?
+                                <StyledForm>
+                                    <Formik
+                                        initialValues={initialValues}
+                                        validationSchema={validationSchema}
+                                        onSubmit={submitMessage}
+                                        component={ContactForm}
+                                    />
+                                </StyledForm>
+                                : <StyledSuccess>
+                                    <h3>Message Sent!</h3>
+                                    <img src={SuccessImage} alt='tick!' />
+                                </StyledSuccess>
+                            }
+                            <ToastContainer
+                                position="bottom-left"
+                                bodyClassName="toast"
+                                autoClose={3000}
+                                closeButton={false}
                             />
-                        </StyledForm>
-                        : <StyledSuccess>
-                            <h3>Message Sent!</h3>
-                            <img src={SuccessImage} alt='tick!' />
-                        </StyledSuccess>
+                        </>
                     }
                 </div>
                 <div className="contactRight">
--- src/Views/ContactStyle.js
+++ src/Views/ContactStyle.js
@@ -90,3 +90,10 @@ h3 {
         height: auto;
     }
 `
+export const StyledSpinner = styled.div`
+width: 100%;
+height: 100%;
+display: flex;
+justify-content: center;
+align-items: center;
+`
