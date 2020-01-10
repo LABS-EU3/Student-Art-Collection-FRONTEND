f4a48add66e5aa6b2fd284fc29b3c11d87396024
Petar Scepanovic <hiborija@gmail.com>
2020-01-10 16:33:45 +0100

    more refactoring

--- src/Views/SchoolsDashboard/SchoolsDasboardStyle.js
+++ src/Views/SchoolsDashboard/SchoolsDasboardStyle.js
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
 
 export const MainContainer = styled.section`
-	width: 900px;
 	box-sizing: border-box;
-	height: 80%;
+	width: 80%;
 	padding: 1rem;
 	margin: 0 auto;
 `;
@@ -12,11 +11,9 @@ export const SchoolsDashboardContainer = styled.section`
 	margin-top: 3rem;
 	display: grid;
 	padding: 1 rem;
-	width: 100%;
-	max-width: 800px;
 	grid-gap: 5%;
 	grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-	${'' /* grid-template-rows: repeat(2, 100px); */} grid-auto-rows: 100px;
+	grid-auto-rows: 100px;
 `;
 
 export const CollectionItemContainer = styled.div`
