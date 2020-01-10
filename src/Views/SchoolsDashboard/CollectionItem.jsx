ff438bdc04150b3bfe0b7e79fd067c3967fbcf71
Petar Scepanovic <hiborija@gmail.com>
2020-01-10 16:22:08 +0100

    some refactoring

--- src/Views/SchoolsDashboard/CollectionItem.jsx
+++ src/Views/SchoolsDashboard/CollectionItem.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import {
-	CollectionItemContainer,
-} from './SchoolsDasboardStyle';
+import React from 'react';
+import { CollectionItemContainer } from './SchoolsDasboardStyle';
 
-function CollectionItem() {
-    return (
-        <CollectionItemContainer/>
-    )
+function CollectionItem (){
+	return (
+		<CollectionItemContainer>
+			<p>Order</p><br/>
+			<p>01.05.2020</p><br/>
+			<p>$45</p><br/>
+			<p>Dispatched</p><br/>
+		</CollectionItemContainer>
+	);
 }
 
-export default CollectionItem
+export default CollectionItem;
--- src/Views/SchoolsDashboard/SchoolsDasboardStyle.js
+++ src/Views/SchoolsDashboard/SchoolsDasboardStyle.js
@@ -4,23 +4,26 @@ export const MainContainer = styled.section`
 	width: 900px;
 	box-sizing: border-box;
 	height: 80%;
-	padding: 10px;
+	padding: 1rem;
 	margin: 0 auto;
 `;
 
 export const SchoolsDashboardContainer = styled.section`
 	margin-top: 3rem;
 	display: grid;
+	padding: 1 rem;
 	width: 100%;
 	max-width: 800px;
-	grid-gap: 10px;
+	grid-gap: 5%;
 	grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
 	${'' /* grid-template-rows: repeat(2, 100px); */} grid-auto-rows: 100px;
 `;
 
 export const CollectionItemContainer = styled.div`
-	width: 40%;
+	width: 70%;
 	display: flex;
-	height: 80%;
+	flex-direction: column;
+	height: 100%;
+	padding: 1rem;
 	border: 1px solid black;
 `;
