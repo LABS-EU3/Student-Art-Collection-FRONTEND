34d3757c397c6c625561ba573e669717d13d0987
Petar Scepanovic <hiborija@gmail.com>
2020-01-10 16:47:10 +0100

    update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import ConfirmationSuccess from './Views/ConfirmationSuccess';
 import PasswordResetSent from './Views/PasswordResetSent';
 import Login from './Views/Login';
 import Dashboard from './Views/Dashboard'
-import SchoolsDashboard from './Views/SchoolsSoldItems/SchoolsSoldItems'
+import SchoolsSoldItems from './Views/SchoolsSoldItems/SchoolsSoldItems'
 import PasswordReset from './Components/resetPassword/PasswordReset';
 
 // global style
@@ -25,7 +25,7 @@ function App() {
       <GlobalStyle />
       <Navbar />
       <Route exact path="/" component={LandingPage} />
-      <Route exact path="/schoolsdashboard" component={SchoolsDashboard} />
+      <Route exact path="/schoolssolditems" component={SchoolsSoldItems} />
       <Route path="/resetpasswordrequest" component={ResetPasswordForm} />
       <Route path="/confirmation" component={ConfirmationSent} />
       <Route path="/success" component={ConfirmationSuccess} />
