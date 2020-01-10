9e781f65cd028aff68b6c98fd6ce8792cfa32325
Petar Scepanovic <hiborija@gmail.com>
2020-01-10 12:47:41 +0100

    deleted most of the files, cleaned up

--- src/Views/SchoolsDashboard/SchoolsDasboardStyle.js
+++ src/Views/SchoolsDashboard/SchoolsDasboardStyle.js
@@ -1,84 +1,22 @@
 import styled from 'styled-components';
 
 export const MainContainer = styled.section`
-	width:900px;
+	width: 900px;
 	box-sizing: border-box;
 	height: 80%;
 	padding: 10px;
 	margin: 0 auto;
-
 `;
 
 export const SchoolsDashboardContainer = styled.section`
-    margin-top: 3rem;
+	margin-top: 3rem;
 	display: grid;
-	width:100%;
+	width: 100%;
 	max-width: 800px;
-    grid-gap: 10px;
-    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-    ${'' /* grid-template-rows: repeat(2, 100px); */}
-    grid-auto-rows: 100px;
-
-
+	grid-gap: 10px;
+	grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+	${'' /* grid-template-rows: repeat(2, 100px); */} grid-auto-rows: 100px;
 `;
-
-export const StyledSidebar = styled.aside`
-	width: 30%;
-	display: flex;
-	flex-direction: column;
-	background: white;
-`;
-
-export const StyledNavItem = styled.div`
-	display: flex;
-	align-items: center;
-
-	a {
-		${'' /* display: block; */} width: 100%;
-		font-size: 1.5rem;
-		padding: 2rem;
-		padding-left: 1.4rem;
-		border-bottom: 1px solid black;
-		text-decoration: none;
-		color: goldenrod;
-		line-height: 1.2rem;
-	}
-
-	&:hover a {
-		color: black;
-		background: #dbf2ff;
-	}
-`;
-
-export const ArrowWrapper = styled.div`
-	padding: 2rem;
-	display: inline-block;
-	vertical-align: middle;
-	-webkit-transform: perspective(1px) translateZ(0);
-	transform: perspective(1px) translateZ(0);
-	box-shadow: 0 0 1px rgba(0, 0, 0, 0);
-	-webkit-transition-duration: 0.5s;
-	transition-duration: 0.5s;
-	cursor: pointer;
-
-	&:hover,
-	&:focus,
-	&:active {
-		-webkit-transform: scale(1.2);
-		transform: scale(1.2);
-		-webkit-transition-timing-function: cubic-bezier(0.47, 2.02, 0.31, -0.36);
-		transition-timing-function: cubic-bezier(0.47, 2.02, 0.31, -0.36);
-	}
-`;
-
-export const NavItemsWrapper = styled.div`
-	display: flex;
-	width: 90%;
-	flex-direction: column;
-	align-content: flex-start;
-`;
-
-export const MainCollectionItemContainer = styled.div``;
 
 export const CollectionItemContainer = styled.div`
 	width: 20%;
@@ -86,9 +24,4 @@ export const CollectionItemContainer = styled.div`
 	height: 35%;
 	border: 1px solid black;
 	margin-left: 1.5rem;
-
-	${'' /* flex-direction: column;
-	height: 350px;
-	align-items: center;
-	position: relative; */}
 `;
--- src/Views/SchoolsDashboard/SchoolsDashboard.jsx
+++ src/Views/SchoolsDashboard/SchoolsDashboard.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { SchoolsDashboardContainer, MainContainer } from './SchoolsDasboardStyle';
 import CollectionItem from './CollectionItem';
-import { ReactComponent as LeftArrow } from '../../Assets/images/left_arrow.svg';
-import Sidebar from './Sidebar';
 
 function SchoolsDashboard (){
 	return (
 		<MainContainer>
 			<SchoolsDashboardContainer>
-				{/* <Sidebar /> */}
 				<CollectionItem />
 				<CollectionItem />
 				<CollectionItem />
@@ -20,11 +17,6 @@ function SchoolsDashboard (){
 				<CollectionItem />
 				<CollectionItem />
 				<CollectionItem />
-				
-
-				{/* <ArrowWrapper>
-            <LeftArrow width="70%"/>
-        </ArrowWrapper> */}
 			</SchoolsDashboardContainer>
 		</MainContainer>
 	);
