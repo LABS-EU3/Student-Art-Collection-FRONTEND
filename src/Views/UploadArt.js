6cfb708853e13ebbc49f58f5289987e84b992c69
Brenda Joshua <brendajosh.bj@gmail.com>
2020-01-14 11:35:54 +0300

    set fields state

--- src/Views/UploadArt.js
+++ src/Views/UploadArt.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
-import { Formik } from 'formik';
+import { Formik, Field } from 'formik';
 
 
 // const BackgroundStyled = styled.div`
@@ -19,60 +19,60 @@ flex-direction: column;
 margin-top: 20px;
 `
 
-const File = styled.input`
-width: 900px;
-height: 100px;
-margin-left: 20px;
-padding: 12px 20px;
-box-sizing: border-box;
-border: 2px solid #ccc;
-border-radius: 4px;
-background-color: #f8f8f8;
-resize: none;
-margin-bottom: 20px;
-`
-const Description = styled.input`
-width: 900px;
-height: 120px;
-padding: 12px 20px;
-box-sizing: border-box;
-resize: none;
-margin-left: 20px;
-margin-bottom: 20px;
-`
+// const File = styled.input`
+// width: 900px;
+// height: 100px;
+// margin-left: 20px;
+// padding: 12px 20px;
+// box-sizing: border-box;
+// border: 2px solid #ccc;
+// border-radius: 4px;
+// background-color: #f8f8f8;
+// resize: none;
+// margin-bottom: 20px;
+// `
+// const Description = styled.input`
+// width: 900px;
+// height: 120px;
+// padding: 12px 20px;
+// box-sizing: border-box;
+// resize: none;
+// margin-left: 20px;
+// margin-bottom: 20px;
+// `
 
-const Details = styled.input`
-width:450px;
-height: 50px;
-padding: 12px 20px;
-margin-left: 20px;
-margin-bottom: 20px;
-`
+// const Details = styled.input`
+// width:450px;
+// height: 50px;
+// padding: 12px 20px;
+// margin-left: 20px;
+// margin-bottom: 20px;
+// `
 
-const ArtName = styled.div`
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-margin-bottom: 20px;
-`
-const Extras = styled.div`
-display:flex;
-flex-direction: row;
-justify-content: space-between;
-flex-wrap: wrap;
-`
-const Parameters = styled.input`
-width: 200px;
-height: 40px;
-padding: 12px 20px;
-margin-left: 20px;
-`
-const Values = styled.div`
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-margin-bottom: 20px;
-`
+// const ArtName = styled.div`
+// display: flex;
+// flex-direction: row;
+// justify-content: space-between;
+// margin-bottom: 20px;
+// `
+// const Extras = styled.div`
+// display:flex;
+// flex-direction: row;
+// justify-content: space-between;
+// flex-wrap: wrap;
+// `
+// const Parameters = styled.input`
+// width: 200px;
+// height: 40px;
+// padding: 12px 20px;
+// margin-left: 20px;
+// `
+// const Values = styled.div`
+// display: flex;
+// flex-direction: row;
+// justify-content: space-between;
+// margin-bottom: 20px;
+// `
 const Button = styled.input`
 background: #FF9B04;
 display: inline-block;
@@ -107,6 +107,13 @@ function UploadArt() {
  const [title, setTitle] = useState("");
  const [file, setFile] = useState(null);
  const [desc, setDesc] = useState("");
+ const [width, setWidth] = useState("");
+ const [height, setHeight] = useState("");
+ const [quantity, setQuantity] = useState("");
+ const [materials, setMaterials] = useState("");
+ const [style, setStyle] = useState("");
+ const [medium, setMedium] = useState("");
+ const [price, setPrice] = useState("");
 
  function uploadWithFormData(){
 
@@ -114,78 +121,73 @@ function UploadArt() {
 
 
  return (
-    // <BackgroundStyled>
+
+    <Formik>
  <div className="UploadArt">
  <Form>
  <label>
- <File type="file" name="file" onChange={(e) => setFile(e.target.files[0])} />
+ <Field type="file" name="file" onChange={(e) => setFile(e.target.files[0])} />
  </label>
 
-<ArtName>
  <label>
- <Details type="text" vaue={title} 
+ <Field type="text" vaue={title} 
  onChange={(e) => { setTitle(e.target.value )}} 
  placeholder="Title" />
  </label>
 
  <label>
- <Details type="text" vaue={title} 
+ <Field type="text" vaue={title} 
  onChange={(e) => { setTitle(e.target.value )}} 
  placeholder="Artist Name" />
  </label>
- </ArtName>
 
  <label>
- <Description type="text" vaue={title} 
- onChange={(e) => { setTitle(e.target.value )}} 
+ <Field type="text" vaue={desc} 
+ onChange={(e) => { setDesc(e.target.value )}} 
  placeholder="Description" />
  </label>
 
-<Values>
  <label>
- <Parameters type="text" vaue={title} 
- onChange={(e) => { setTitle(e.target.value )}} 
+ <Field type="text" vaue={width} 
+ onChange={(e) => { setWidth(e.target.value )}} 
  placeholder="Width" />
  </label>
 
  <label>
- <Parameters type="text" vaue={title} 
- onChange={(e) => { setTitle(e.target.value )}} 
+ <Field type="text" vaue={height} 
+ onChange={(e) => { setHeight(e.target.value )}} 
  placeholder="Height" />
  </label>
 
  <label>
- <Parameters type="text" vaue={title} 
- onChange={(e) => { setTitle(e.target.value )}} 
+ <Field type="text" vaue={quantity} 
+ onChange={(e) => { setQuantity(e.target.value )}} 
  placeholder="Quantity" />
  </label>
- </Values>
 
-<Extras>
  <label>
- <Details type="text" vaue={title} 
- onChange={(e) => { setTitle(e.target.value )}} 
+ <Field type="text" vaue={materials} 
+ onChange={(e) => { setMaterials(e.target.value )}} 
  placeholder="Materials" />
  </label>
 
  <label>
- <Details type="text" vaue={title} 
- onChange={(e) => { setTitle(e.target.value )}} 
+ <Field type="text" vaue={style} 
+ onChange={(e) => { setStyle(e.target.value )}} 
  placeholder="Style" />
  </label>
 
  <label>
- <Details type="text" vaue={title} 
- onChange={(e) => { setTitle(e.target.value )}} 
+ <Field type="text" vaue={medium} 
+ onChange={(e) => { setMedium(e.target.value )}} 
  placeholder="Medium" />
  </label>
 
  <label>
- <Details type="text" vaue={title} 
- onChange={(e) => { setTitle(e.target.value )}} 
+ <Field type="text" vaue={price} 
+ onChange={(e) => { setPrice(e.target.value )}} 
  placeholder="Price" />
  </label>
- </Extras>
 
  <h1>I agree to the terms and conditions and have all relevant permissions to upload and sell this art <input type="checkbox"></input></h1>
 
@@ -193,7 +195,7 @@ function UploadArt() {
  
  </Form>
  </div>
-//  </BackgroundStyled>
+ </Formik>
  );
 }
 
