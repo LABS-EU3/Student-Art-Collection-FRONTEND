a096a733220080c54e8e419cb9cb107ce1fae67d
Petar Scepanovic <hiborija@gmail.com>
2020-01-21 19:01:19 +0100

    start adding suspense, wrapping the components, added switch

--- src/App.js
+++ src/App.js
@@ -1,52 +1,64 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import './App.css';
-import Navbar from './Components/NavBar';
-import ErrorBoundary from './Components/error-boundary/error-boundary.component'
-import LandingPage from './Views/LandingPage';
-import Register from './Views/Register';
-import ConfirmationSent from './Views/ConfirmationSent';
-import ConfirmationSuccess from './Views/ConfirmationSuccess';
-import PasswordResetSent from './Views/PasswordResetSent';
-import Login from './Views/Login';
-import Dashboard from './Views/Dashboard'
-import PasswordReset from './Components/resetPassword/PasswordReset';
-import ContactPage from './Views/Contact';
-import BrowseArt from './Views/BrowseArt';
-import ArtViewModal from './Views/ArtViewModal';
-
-
 // global style
 import GlobalStyle from './Styles/GlobalStyle';
 // import theme
 import theme from './Styles/Theme';
 import ResetPasswordForm from './Components/resetPassword/ResetPasswordForm';
 
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-       <Navbar />
-      <Switch>
-      <ErrorBoundary>
-      <Route path="/browse" component={ArtViewModal} />
-      <Route exact path="/" component={LandingPage} />
-      <Route path='/contact' component={ContactPage} />
-      <Route path="/resetpasswordrequest" component={ResetPasswordForm} />
-      <Route path="/confirmation" component={ConfirmationSent} />
-      <Route path="/success" component={ConfirmationSuccess} />
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={Register} />
-      <Route path="/myaccount" component={Dashboard} />
-      <Route path="/selling" component={Dashboard} />
-      <Route path="/resetpasswordsent" component={PasswordResetSent} />
-      <Route path="/resetpassword" component={PasswordReset} />
-      <Route path="/browse" component={BrowseArt} />
-      </ErrorBoundary>
-      </Switch>
-    </ThemeProvider>
-  );
+import './App.css';
+import Spinner from './Components/Spinner';
+import Navbar from './Components/NavBar';
+import ErrorBoundary from './Components/error-boundary/error-boundary.component';
+// import LandingPage from './Views/LandingPage';
+// import Register from './Views/Register';
+// import ConfirmationSent from './Views/ConfirmationSent';
+// import ConfirmationSuccess from './Views/ConfirmationSuccess';
+// import PasswordResetSent from './Views/PasswordResetSent';
+import Login from './Views/Login';
+import Dashboard from './Views/Dashboard';
+import PasswordReset from './Components/resetPassword/PasswordReset';
+import ContactPage from './Views/Contact';
+import BrowseArt from './Views/BrowseArt';
+import ArtViewModal from './Views/ArtViewModal';
+
+const LandingPage = lazy(() => import('./Views/LandingPage'));
+const Register = lazy(() => import('./Views/Register'));
+const ConfirmationSent = lazy(() => import('./Views/ConfirmationSent'));
+const ConfirmationSuccess = lazy(() => import('./Views/PasswordResetSent'));
+const PasswordResetSent = lazy(() => import('./Views/PasswordResetSent'));
+
+function App (){
+	return (
+		<ThemeProvider theme={theme}>
+			<GlobalStyle />
+			<Navbar />
+			<Switch>
+				<ErrorBoundary>
+					<Route path="/browse" component={ArtViewModal} />
+					<Suspense fallback={Spinner}>
+						<Route exact path="/" component={LandingPage} />
+						<Route path="/signup" component={Register} />
+						<Route path="/confirmation" component={ConfirmationSent} />
+						<Route path="/success" component={ConfirmationSuccess} />
+						<Route path="/resetpasswordsent" component={PasswordResetSent} />
+					</Suspense>
+
+					<Route path="/contact" component={ContactPage} />
+					<Route path="/resetpasswordrequest" component={ResetPasswordForm} />
+
+					<Route path="/login" component={Login} />
+
+					<Route path="/myaccount" component={Dashboard} />
+					<Route path="/selling" component={Dashboard} />
+
+					<Route path="/resetpassword" component={PasswordReset} />
+					<Route path="/browse" component={BrowseArt} />
+				</ErrorBoundary>
+			</Switch>
+		</ThemeProvider>
+	);
 }
 
 export default App;
