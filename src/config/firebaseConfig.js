6898131720ba8807530179f1cd7f44a4fba29bca
Rory Flint <roryflintphoto@gmail.com>
2020-01-23 11:59:59 +0000

    Add firebase config file

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API,
+  authDomain: "artfunder.firebaseapp.com",
+  databaseURL: "https://artfunder.firebaseio.com",
+  projectId: "artfunder",
+  storageBucket: "artfunder.appspot.com",
+  messagingSenderId: "128794234534",
+  appId: "1:128794234534:web:942a0396d593cd4c2f2e67",
+  measurementId: "G-B01FWLH87V"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
