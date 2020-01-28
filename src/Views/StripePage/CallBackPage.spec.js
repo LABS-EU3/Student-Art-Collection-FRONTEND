b2ec612b82685de1c821bb0bed3e13f3e6a3f226
iamcristos <nmereginivincent@gmail.com>
2020-01-28 15:22:44 +0100

    wrote test for registration page

--- src/Views/StripePage/CallBackPage.spec.js
+++ src/Views/StripePage/CallBackPage.spec.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import CallBackPage from './CallBackPage';
-import {render} from '@testing-library/react';
 import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import axiosMock from 'jest-mock-axios';
