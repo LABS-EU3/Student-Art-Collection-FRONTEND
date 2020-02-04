85f83a47672557ce5ba685187089415e60fea47b
Pere <pere.solaclaver@gmail.com>
2020-02-04 11:41:05 +0000

    Add spinner instead of loading message in schoolart.js file

--- src/Views/SchoolArt.js
+++ src/Views/SchoolArt.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actionCreators from '../store/Actions/actionCreators';
@@ -6,46 +6,55 @@ import * as actionCreators from '../store/Actions/actionCreators';
 // COMPONENTS
 import SchoolArtCard from '../Components/SchoolComponents/SchoolArtCard';
 import SchoolPageHero from '../Components/SchoolComponents/SchoolPageHero';
+import Spinner from '../Components/Spinner';
 
 // HELPERS
 import { axiosWithBase } from '../AxiosCustom';
 
 // STYLING
-import { StyledContainer } from '../Components/BrowseComponents/BrowseArtContainerStyling';
+import {
+  StyledContainer,
+  StyledEmptyContainer
+} from '../Components/BrowseComponents/BrowseArtContainerStyling';
 
 const SchoolArt = ({ artSelectedSchool, setArtSelectedSchool, match }) => {
+  const [spinning, setSpinning] = useState(true);
   const { id } = match.params;
   useEffect(() => {
     axiosWithBase()
       .get(`/art/school/art/${id}`)
       .then(res => {
-        debugger;
         setArtSelectedSchool(res.data);
+        setSpinning(false);
       })
       .catch(error => {
-        debugger;
+        setSpinning(false);
       });
   }, []);
-  if (!artSelectedSchool || artSelectedSchool.length === 0) {
-    return <div>Loading...</div>;
-  } else {
+
+  if (spinning) {
     return (
-      <>
-        <SchoolPageHero />
-        <StyledContainer>
-          <div className="grid-row">
-            {artSelectedSchool === 'No Art for this school at the moment' ? (
-              <div>Sorry, this school has no art</div>
-            ) : (
-              artSelectedSchool.map(art => {
-                return <SchoolArtCard art={art} key={art._id} />;
-              })
-            )}
-          </div>
-        </StyledContainer>
-      </>
+      <StyledEmptyContainer>
+        <Spinner />
+      </StyledEmptyContainer>
     );
   }
+  return (
+    <>
+      <SchoolPageHero />
+      <StyledContainer>
+        <div className="grid-row">
+          {artSelectedSchool === 'No Art for this school at the moment' ? (
+            <div>Sorry, this school has no art</div>
+          ) : (
+            artSelectedSchool.map(art => {
+              return <SchoolArtCard art={art} key={art._id} />;
+            })
+          )}
+        </div>
+      </StyledContainer>
+    </>
+  );
 };
 
 export default withRouter(connect(state => state, actionCreators)(SchoolArt));
